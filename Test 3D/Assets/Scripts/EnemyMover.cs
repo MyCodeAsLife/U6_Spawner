1b31c6786d76b665bf4448ec0b9bafb012a8b1a4
Vladimir <w112vovan@mail.ru>
2024-02-09 13:58:41 +0500

    Version 1.0

---
+++ Test 3D/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementDirrection;
+    [SerializeField] private float _movementSpeed;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        transform.rotation = Quaternion.LookRotation(_movementDirrection);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        _movementDirrection = direction;
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
+        _animator.SetFloat("Speed", _movementSpeed);
+    }
+}
---
+++ Test 3D/Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPonts;
+    [SerializeField] private Dummy _enemy;
+    [SerializeField] private float _timeBetweenSpawn = 2;
+    [SerializeField] private float _lifetime = 20;
+
+    private float _minValue = -1;
+    private float _maxValue = 1;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(EnemySpawn), _timeBetweenSpawn, _timeBetweenSpawn);
+    }
+
+    private void EnemySpawn()
+    {
+        int randomIndex = Random.Range(0, _spawnPonts.Length);
+        Vector3 direction = new Vector3(Random.Range(_minValue, _maxValue), 0, Random.Range(_minValue, _maxValue));
+        Dummy enemy = Instantiate(_enemy, _spawnPonts[randomIndex].position, Quaternion.identity);
+
+        if (enemy.TryGetComponent(out EnemyMover enemyMover))
+            enemyMover.SetDirection(direction);
+
+        Destroy(enemy, _lifetime);
+    }
+}
